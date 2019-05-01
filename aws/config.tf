313f8cb21b7d20ce3ea8a74843ef4034d61ee72a
Jam Risser <jam@codejam.ninja>
2019-05-01 00:32:33 -0500

    Split up files

--- aws/config.tf
+++ aws/config.tf
@@ -1,3 +1,22 @@
+# terraform {
+#   backend "s3" {
+#     bucket = "<SOME_BUCKET>"
+#     key    = "<SOME_DOMAIN>"
+#     region = "<SOME_REGION>"
+#   }
+# }
+# data "terraform_remote_state" "network" {
+#   backend = "s3"
+#   config {
+#     bucket = "<SOME_BUCKET>"
+#     key    = "${var.name}.${var.domain}/ipa"
+#     region = "${var.region}"
+#   }
+# }
+provider "aws" {
+  region                  = "${var.region}"
+  shared_credentials_file = "~/.aws/credentials"
+}
 variable "region" {
   type    = "string"
   default = "us-west-2"
@@ -24,10 +43,4 @@ variable "rancher_version" {
 variable "docker_version" {
   type    = "string"
   default = "18.09.4-ce"
-}
-variable "cloudflare_token" {
-  type    = "string"
-}
-variable "cloudflare_email" {
-  type    = "string"
 }

4e29f89693cccfadfde61435af5f7622e7305486
Jam Risser <jam@codejam.ninja>
2019-06-04 08:31:38 -0500

    Setup moderngreek terraform config

--- aws/config.tf
+++ aws/config.tf
@@ -1,25 +1,25 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "<SOME_BUCKET>"
-#     key    = "<SOME_DOMAIN>"
-#     region = "<SOME_REGION>"
-#   }
-# }
-# data "terraform_remote_state" "network" {
-#   backend = "s3"
-#   config = {
-#     bucket = "<SOME_BUCKET>"
-#     key    = "${var.name}.${var.domain}/ipa"
-#     region = "${var.region}"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "moderngreek-terraform"
+    key    = "orch.moderngreek.us/orch"
+    region = "us-east-1"
+  }
+}
+data "terraform_remote_state" "network" {
+  backend = "s3"
+  config = {
+    bucket = "moderngreek-terraform"
+    key    = "${var.name}.${var.domain}/orch"
+    region = "${var.region}"
+  }
+}
 provider "aws" {
   region                  = "${var.region}"
   shared_credentials_file = "~/.aws/credentials"
 }
 variable "region" {
   type    = "string"
-  default = "us-west-2"
+  default = "us-east-1"
 }
 variable "name" {
   type    = "string"
@@ -27,6 +27,7 @@ variable "name" {
 }
 variable "domain" {
   type    = "string"
+  default = "moderngreek.us"
 }
 variable "volume_size" {
   type    = "string"
